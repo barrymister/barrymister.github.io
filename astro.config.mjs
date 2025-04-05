86f8253a224e566f9e7df460581ef632a82c4ea1
Barry Mister <barry.mister@gmail.com>
2025-04-05 18:20:33 -0400

    switch to branch-based deployment using /docs

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import mdx from '@astrojs/mdx';
 
 export default defineConfig({
   site: 'https://barrymister.com',
+  outDir: './docs',  // GitHub Pages will serve from /docs
   integrations: [tailwind(), mdx()],
   output: 'static',
   build: {
